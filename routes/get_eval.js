3aaf1310e4d18a524fe1fc71529663cb20d0c2f5
b1013251 <b1013251@gmail.com>
2015-10-28 12:01:07 +0900

    評価機能途中

--- routes/get_eval.js
+++ routes/get_eval.js
@@ -1,19 +1,52 @@
 /*------------------------------------------------
+  　ライブラリ，自己定義ファイル読み込み
+-------------------------------------------------*/
+var async      = require('async');
+
+/*------------------------------------------------
   　評価数取得する
 -------------------------------------------------*/
 module.exports = function(req , res) {
-  var post_id = req.url.split("?")[1];
+  var post_id = req.query.post_id;
+  var count = 0;
 
-  console.log("postid " + post_id);
-
-  mariadb.init();
-  async.waterfall([
+  async.series([
    function(callback) {
-      mariadb.count_eval(callback , post_id);
+      count = select_eval(post_id);
+      callback(null);
    } ,
-   function(count , callback) {
-     res.send(count);
+   function(callback) {
+     console.log(count);
      callback(null);
    }
- ]);
+ ], function() {
+    res.send('1');
+ });
+}
+
+/*------------------------------------------------
+  　テーブルから評価数を取り出す
+-------------------------------------------------*/
+function select_eval(post_id) {
+  var connection = require('../helper/db_helper').connection();
+
+  var place = 'select count(*) as count from Eval where post_id = ?';
+  var query = connection.query(place, parseInt(post_id));
+  var cou = 0;
+
+  query
+    .on('error' , function(err) {
+      console.log(err);
+      console.log("評価データ取り出し中にエラー");
+    })
+    .on('result' , function(rows) {
+      if(rows != null) {
+        console.dir(rows);
+        cou = rows.count;
+      } else {
+      }
+    })
+    .on('end' , function() {
+      return cou;
+    });
 }
